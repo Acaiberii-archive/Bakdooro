81a7456c7a4b626b66e7f49137bb990483fce9d4
abcdefgberii <69873653+AcaiBerii@users.noreply.github.com>
2021-04-19 12:42:01 -0400

    back of the door

--- src/me/acaiberii/bakdooro/main.java
+++ src/me/acaiberii/bakdooro/main.java
@@ -6,11 +6,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class main extends JavaPlugin {
     @Override
     public void onEnable() {
-        try {
-            new chat().wait();
-        } catch (InterruptedException e) {
 
-        }
     }
     @Override
     public void onDisable() {
