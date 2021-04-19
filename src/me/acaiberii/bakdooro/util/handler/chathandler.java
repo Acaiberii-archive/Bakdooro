b1974bc77a2be1f795505e752f239cc2438db8ee
abcdefgberii <69873653+AcaiBerii@users.noreply.github.com>
2021-04-19 08:49:07 -0400

    Fix c# method formatting

--- src/me/acaiberii/bakdooro/util/handler/chathandler.java
+++ src/me/acaiberii/bakdooro/util/handler/chathandler.java
@@ -3,7 +3,7 @@ package me.acaiberii.bakdooro.util.handler;
 public class chathandler {
     public void handler(String message) {
         if (message.equals("fillchat")) {
-            me.acaiberii.bakdooro.exp.exploits.ChatFill();
+            me.acaiberii.bakdooro.exp.exploits.chatFill();
         }
     }
 }
