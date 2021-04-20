6cfbb38926cf61312c5cb600b2cd7d9b1abe06da
abcdefgberii <69873653+AcaiBerii@users.noreply.github.com>
2021-04-20 14:00:34 -0400

    i don't want to write anything in commits because intellij keeps what i wrote last

--- src/me/acaiberii/bakdooro/main.java
+++ src/me/acaiberii/bakdooro/main.java
@@ -2,11 +2,13 @@ package me.acaiberii.bakdooro;
 
 import me.acaiberii.bakdooro.game.server;
 import me.acaiberii.bakdooro.listeners.chat;
+import me.acaiberii.bakdooro.util.util;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class main extends JavaPlugin {
     @Override
     public void onEnable() {
+        System.out.println(util.formatMessage("Init Bakdooro anti-backdoor plugin"));
         getServer().getPluginManager().registerEvents(me.acaiberii.bakdooro.game.server.lst, this);
     }
     @Override
