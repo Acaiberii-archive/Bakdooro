7dc48be7dced319806331b174fd430beebd6aebb
abcdefgberii <69873653+AcaiBerii@users.noreply.github.com>
2021-04-21 09:59:45 -0400

    i don't want to write anything in commits because intellij keeps what i wrote last

--- src/me/acaiberii/bakdooro/commands/misc/misc.java
+++ src/me/acaiberii/bakdooro/commands/misc/misc.java
@@ -1,7 +1,9 @@
 package me.acaiberii.bakdooro.commands.misc;
 
+import static me.acaiberii.bakdooro.game.server.srv;
+
 public class misc {
     public static void Floppa() {
-
+        srv.broadcastMessage("Floppa ontoppa");
     }
 }
